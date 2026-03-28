8e039d7205be52425d2a22e88282c635bb79eace
Gustavo Ferreira <luis.guga98@hotmail.com>
2026-03-28 00:01:24 -0300

    2 versão

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const toggle = document.getElementById("themeToggle");
 
+// Verifica se já existe preferência salva
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggle.textContent = "☀️";
+}
+
+// Evento de clique
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggle.textContent = "🌙";
+  }
 });
